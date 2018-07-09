55d1cbcfc2eb1b8957603eb5a9d754782cede4cf
Philip Johnson <johnson@hawaii.edu>
2018-07-08 15:22:53 -1000

    started on simple bio form.

--- app/src/simplebioeditor/SimpleBioEditor.jsx
+++ app/src/simplebioeditor/SimpleBioEditor.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import SimpleSchema from 'simpl-schema';
+
 
 export default class SimpleBioEditor extends React.Component {
+
+  submit(data) {
+    console.log('submit', data);
+  }
+
   render() {
+    const formSchema = new SimpleSchema({
+      name: String,
+      label: String,
+      picture: String,
+      email: String,
+      phone: String,
+      website: String,
+      summary: String,
+      address: String,
+      city: String,
+      countryCode: String,
+      region: String,
+      githubUsername: String,
+      githubUrl: String,
+      linkedinUsername: String,
+      linkedInUrl: String,
+    });
     return (
       <div>
         <p>bio editor goes here.</p>
