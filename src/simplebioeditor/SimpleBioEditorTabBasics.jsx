52fc870ce44652d568a1018d2607aabac9d9b0ff
Adi <adityak@hawaii.edu>
2018-09-21 08:41:40 -1000

    Basics Tab No longer crashes on empty field

--- src/simplebioeditor/SimpleBioEditorTabBasics.jsx
+++ src/simplebioeditor/SimpleBioEditorTabBasics.jsx
@@ -13,37 +13,68 @@ export default class SimpleBioEditorTabBasics extends React.Component {
   constructor(props) {
     super(props);
     this.submit = this.submit.bind(this);
+
     this.state = { model: {} };
-    this.state.model.name = this.props.bio.basics.name;
-    this.state.model.label = this.props.bio.basics.label;
-    this.state.model.picture = this.props.bio.basics.picture;
-    this.state.model.email = this.props.bio.basics.email;
-    this.state.model.phone = this.props.bio.basics.phone;
-    this.state.model.website = this.props.bio.basics.website;
-    this.state.model.summary = this.props.bio.basics.summary;
-    this.state.model.address = this.props.bio.basics.location.address;
-    this.state.model.postalCode = this.props.bio.basics.location.postalCode;
-    this.state.model.city = this.props.bio.basics.location.city;
-    this.state.model.region = this.props.bio.basics.location.region;
-    this.state.model.countryCode = this.props.bio.basics.location.countryCode;
+    this.state.model.name = '';
+    this.state.model.label = '';
+    this.state.model.picture = '';
+    this.state.model.email = '';
+    this.state.model.phone = '';
+    this.state.model.website = '';
+    this.state.model.summary = '';
+    this.state.model.address = '';
+    this.state.model.postalCode = '';
+    this.state.model.city = '';
+    this.state.model.region = '';
+    this.state.model.countryCode = '';
+
+    if(this.props.bio.basics !== undefined) {
+      if (this.props.bio.basics.name !== undefined) this.state.model.name = this.props.bio.basics.name ;
+      if (this.props.bio.basics.label !== undefined) this.state.model.label = this.props.bio.basics.label ;
+      if (this.props.bio.basics.picture !== undefined) this.state.model.picture = this.props.bio.basics.picture ;
+      if (this.props.bio.basics.email !== undefined) this.state.model.email = this.props.bio.basics.email ;
+      if (this.props.bio.basics.phone !== undefined) this.state.model.phone = this.props.bio.basics.phone ;
+      if (this.props.bio.basics.website !== undefined) this.state.model.website = this.props.bio.basics.website ;
+      if (this.props.bio.basics.summary !== undefined) this.state.model.summary = this.props.bio.basics.summary ;
+      if (this.props.bio.basics.address !== undefined) this.state.model.address = this.props.bio.basics.address;
+      if (this.props.bio.basics.postalCode !== undefined) this.state.model.postalCode = this.props.bio.basics.postalCode ;
+      if (this.props.bio.basics.city !== undefined) this.state.model.city = this.props.bio.basics.city ;
+      if (this.props.bio.basics.region !== undefined) this.state.model.region = this.props.bio.basics.region ;
+      if (this.props.bio.basics.countryCode !== undefined) this.state.model.countryCode = this.props.bio.basics.countryCode ;
+    }
   }
 
   submit(data) {
     const
       { name, label, picture, email, phone, website, summary, address, postalCode, city, countryCode, region } = data;
     const bio = this.props.bio;
+    // this.props.bio.basics = [];
+    // if (this.props.bio.basics.name !== undefined) bio.basics.name = this.props.bio.basics.name ;
+    // if (this.props.bio.basics.label !== undefined) bio.basics.label = this.props.bio.basics.label ;
+    // if (this.props.bio.basics.picture !== undefined) bio.basics.picture = this.props.bio.basics.picture ;
+    // if (this.props.bio.basics.email !== undefined) bio.basics.email = this.props.bio.basics.email ;
+    // if (this.props.bio.basics.phone !== undefined) bio.basics.phone = this.props.bio.basics.phone ;
+    // if (this.props.bio.basics.website !== undefined) bio.basics.website = this.props.bio.basics.website ;
+    // if (this.props.bio.basics.summary !== undefined) bio.basics.summary = this.props.bio.basics.summary ;
+    // if (this.props.bio.basics.address !== undefined) bio.basics.address = this.props.bio.basics.address;
+    // if (this.props.bio.basics.postalCode !== undefined) bio.basics.postalCode = this.props.bio.basics.postalCode ;
+    // if (this.props.bio.basics.city !== undefined) bio.basics.city = this.props.bio.basics.city ;
+    // if (this.props.bio.basics.region !== undefined) bio.basics.region = this.props.bio.basics.region ;
+    // if (this.props.bio.basics.countryCode !== undefined) bio.basics.countryCode = this.props.bio.basics.countryCode ;
+    bio.basics = {};
     bio.basics.name = name || '';
     bio.basics.label = label || '';
     bio.basics.picture = picture || '';
     bio.basics.email = email || '';
     bio.basics.website = website || '';
     bio.basics.summary = summary || '';
-    if (bio.basics.phone) bio.basics.phone = phone;
-    if (bio.basics.location) bio.basics.location.address = address;
-    if (bio.basics.location) bio.basics.location.postalCode = postalCode;
-    if (bio.basics.location) bio.basics.location.city = city;
-    if (bio.basics.location) bio.basics.location.region = region;
-    if (bio.basics.location) bio.basics.location.countryCode = countryCode;
+    bio.basics.phone = phone;
+    bio.basics.location = {};
+    bio.basics.location.address = address;
+    bio.basics.location.postalCode = postalCode;
+    bio.basics.location.city = city;
+    bio.basics.location.region = region;
+    bio.basics.location.countryCode = countryCode;
     writeBioFile(this.props.directory, bio, 'Updated basics section of bio.');
     this.props.handleBioChange(bio);
   }
