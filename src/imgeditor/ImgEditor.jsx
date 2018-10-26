27aab22d4904996dc7fc7ac231fd47580209e2a5
Sae Hyun Song <17700563+saehyuns@users.noreply.github.com>
2018-10-26 13:50:33 -1000

    Updated with new croper

--- src/imgeditor/ImgEditor.jsx
+++ src/imgeditor/ImgEditor.jsx
@@ -35,10 +35,10 @@ export default class ImgEditor extends React.Component {
         imageSize: '',
       },
       crop: {
-        x: 10,
-        y: 10,
-        width: 80,
-        height: 80,
+        x: 0,
+        y: 0,
+        width: 100,
+        height: 100,
         aspect: 0,
       },
     };
@@ -51,12 +51,12 @@ export default class ImgEditor extends React.Component {
     this.toggleAspectRatio = this.toggleAspectRatio.bind(this);
   }
 
-  onImageLoaded(image) {
-    console.log('Loaded Image', image);
+  onImageLoaded() {
+    // console.log('Loaded Image', image);
   }
 
-  onCropComplete(image, crop) {
-    console.log('Crop Completed', crop);
+  onCropComplete() {
+    // console.log('Crop Completed', crop);
   }
 
   onCropChange(crop) {
@@ -64,23 +64,25 @@ export default class ImgEditor extends React.Component {
   }
 
   saveCrop() {
-    let imagePath = this.state.image.path[0].split('/');
+    let fullPath = '';
+    let imagePath = this.state.image.path.split('/');
     imagePath.splice(-1, 1);
     imagePath = imagePath.join('/');
-
-    Jimp.read(this.state.image.path[0], (err, image) => {
+    Jimp.read(this.state.image.path, (err, image) => {
       if (err) throw err;
+      if (!(this.state.properties.imageName.includes('_cropped'))) {
+        fullPath = `${imagePath}/${this.state.properties.imageName}_cropped.png`;
+      } else {
+        fullPath = `${imagePath}/${this.state.properties.imageName}.png`;
+      }
       image.crop(
           ((this.state.crop.x / 100) * this.state.properties.imageWidth),
           ((this.state.crop.y / 100) * this.state.properties.imageHeight),
           ((this.state.crop.width / 100) * this.state.properties.imageWidth),
           ((this.state.crop.height / 100) * this.state.properties.imageHeight),
-      )
-      .write(
-      `${imagePath}/${this.state.properties.imageName}_cropped.jpg`,
-      );
+      ).write(fullPath);
+      this.updateCrop(image, fullPath);
     });
-
     const options = {
       type: 'info',
       title: 'Image has been cropped',
@@ -88,6 +90,37 @@ export default class ImgEditor extends React.Component {
       buttons: ['Okay'],
     };
     dialog.showMessageBox(options);
+  }
+
+  updateCrop(cropImage, cropPath) {
+    console.log(cropImage);
+    console.log(cropPath);
+    if (cropPath !== undefined) {
+      let imgName = cropPath.split('/');
+      imgName = imgName[imgName.length - 1];
+      this.setState({
+        src: cropPath,
+        image: {
+          path: cropPath,
+          border: 'solid black 1px',
+          display: 'none',
+          backgroundColor: 'white',
+        },
+        properties: {
+          imageName: imgName.split('.')[0],
+          imageType: imgName.split('.')[1],
+          imageHeight: cropImage.bitmap.height,
+          imageWidth: cropImage.bitmap.width,
+          imageSize: fs.statSync(cropPath).size / 1000,
+        },
+        crop: {
+          x: 0,
+          y: 0,
+          width: 100,
+          height: 100,
+        },
+      });
+    }
   }
 
   selectImage() {
@@ -98,13 +131,14 @@ export default class ImgEditor extends React.Component {
       buttonLabel: 'Crop',
     }, (fullPath) => {
       if (fullPath !== undefined) {
+        console.log(fullPath);
         const dimensions = sizeOf(fullPath.toString());
         let imgName = fullPath.toString().split('/');
         imgName = imgName[imgName.length - 1];
         this.setState({
           src: fullPath[0],
           image: {
-            path: fullPath,
+            path: fullPath[0],
             border: 'solid black 1px',
             display: 'none',
             backgroundColor: 'white',
@@ -116,6 +150,19 @@ export default class ImgEditor extends React.Component {
             imageWidth: dimensions.width,
             imageSize: fs.statSync(fullPath.toString()).size / 1000,
           },
+          crop: {
+            x: 0,
+            y: 0,
+            width: 100,
+            height: 100,
+          },
+        });
+        let imagePath = this.state.image.path.split('/');
+        imagePath.splice(-1, 1);
+        imagePath = imagePath.join('/');
+        Jimp.read(this.state.image.path, (err, image) => {
+          if (err) throw err;
+          image.write(`${imagePath}/${this.state.properties.imageName}_cropped.png`);
         });
       }
     });
@@ -124,17 +171,9 @@ export default class ImgEditor extends React.Component {
   toggleAspectRatio() {
     this.state.aspect = !this.state.aspect;
     if (this.state.aspect) {
-      this.setState({
-        crop: {
-          aspect: 1,
-        },
-      });
+      this.state.crop.aspect = 1;
     } else {
-      this.setState({
-        crop: {
-          aspect: 0,
-        },
-      });
+      this.state.crop.aspect = 0;
     }
   }
 
@@ -154,13 +193,15 @@ export default class ImgEditor extends React.Component {
             <Header.Subheader>Manage your image for your Techfolio!</Header.Subheader>
           </Header>
         </div>
-        <ReactCrop
-          src={this.state.image.path[0]}
-          crop={this.state.crop}
-          onImageLoaded={this.onImageLoaded}
-          onComplete={this.onCropComplete}
-          onChange={this.onCropChange}
-        />
+        <div style={{ display: 'block', textAlign: 'center' }}>
+          <ReactCrop
+            src={this.state.image.path}
+            crop={this.state.crop}
+            onImageLoaded={this.onImageLoaded}
+            onComplete={this.onCropComplete}
+            onChange={this.onCropChange}
+          />
+        </div>
         <div style={{ textAlign: 'center' }}>
           <div style={{ display: 'inline-block', textAlign: 'center', paddingTop: '2%' }}>
             <Label style={{ color: 'black' }} horizontal>
@@ -195,7 +236,7 @@ export default class ImgEditor extends React.Component {
           <br />
           <Button size="massive" color="blue" onClick={this.saveCrop} fluid>Save Changes</Button>
           <br />
-          <Button size="massive" color="red" fluid>Reset Changes</Button>
+          <Button size="massive" color="red" onClick={this.resetImage} fluid>Reset Changes</Button>
         </div>
       </Container>
     );
