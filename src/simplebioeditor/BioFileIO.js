d42391ac5c43e99ff8c4f5e1cd93f428ce35d5c3
Adi <adityak@hawaii.edu>
2018-10-26 15:39:35 -1000

    Fixed ESLint errors for BioFileIO

--- src/simplebioeditor/BioFileIO.js
+++ src/simplebioeditor/BioFileIO.js
@@ -1,7 +1,20 @@
+import _ from 'underscore';
 import Notify from 'notifyjs'; // WARNING: this import fails when file is loaded in main process.
 import path from 'path';
 
 const fs = require('fs');
+
+export function removeEmptyFields(data) {
+  return _.omit(data, (value) => {
+    let deleteField = false;
+    if (value === null || value === undefined) {
+      deleteField = true;
+    } else if (value.length === 0) {
+      deleteField = true;
+    }
+    return deleteField;
+  });
+}
 
 /**
  * Renderer (remote side) function to write out the bio.json file.
@@ -10,12 +23,12 @@ const fs = require('fs');
  */
 export function writeBioFile(directory, bio, msg = 'File saved') {  //eslint-disable-line
   // Removes fields that are blank from the bio.json object before they are written to disk
-  removeEmptyFields(bio);
+  const fixedBio = removeEmptyFields(bio);
 
   const fileType = '_data';
   const fileName = 'bio.json';
   const filePath = path.join(directory, fileType, fileName);
-  const bioString = JSON.stringify(bio, null, 2);
+  const bioString = JSON.stringify(fixedBio, null, 2);
   fs.writeFile(filePath, bioString, 'utf8', (err) => {
     const body = (err) ? `File write error: ${err}` : msg;
     const notification = new Notify('bio.json', { body, renotify: true, tag: 'bio.json' });
@@ -27,15 +40,6 @@ export function writeBioFile(directory, bio, msg = 'File saved') {  //eslint-dis
   });
 }
 
-export function removeEmptyFields(data) {
-  for (let field in data) {
-    if (data[field] === null || data[field] === undefined) {
-      delete data[field];
-    } else if (data[field].length === 0) {
-      delete data[field];
-    }
-  }
-}
 
 // /**
 //  * Returns the bio.json file as an object if it exists and is parsable, null otherwise.
