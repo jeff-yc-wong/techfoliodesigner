5fca4046ddf7a8406579db849438b6b2d74d23da
blaine-wataru <blainewataru@gmail.com>
2018-11-02 04:48:44 -1000

    Implement Semantic UI, placeholder tag, and Close button

--- src/main/HelpMenu.js
+++ src/main/HelpMenu.js
@@ -10,8 +10,7 @@ function showHelpDialog(label, markdownPath) {
   const htmlText = marked(markdownText);
 
   let finalHtml = fs.readFileSync('../techfoliodesigner/src/help/BaseHelp.html', 'utf-8');
-  finalHtml = finalHtml.concat(htmlText);
-  finalHtml = finalHtml.concat('</body>\n</html>');
+  finalHtml = finalHtml.replace('<placeholder></placeholder>', htmlText);
   fs.writeFileSync('../techfoliodesigner/src/help/HelpMenu.html', finalHtml);
 
   const win = new BrowserWindow({
@@ -55,61 +54,5 @@ export default function buildHelpSubMenu() {
     },
   ];
 
-  // const helpSubMenu = [
-  //   {
-  //     label: 'Getting Started',
-  //     click: () => dialog.showMessageBox({
-  //       message:
-  //               'Welcome to Techfolio Designer!\n\n' +
-  //               '1) To get started, login to GitHub using the Config menu. ' +
-  //               'From here, set the Github repo name of your Techfolio, which will look like this: "(MyGitHubUsername).github.io".\n' + // eslint-disable-line
-  //               '2) Go to "Clone Repo into Directory" in the Config menu, and select the folder ' +
-  //               'where you would like to copy your online Techfolio to your computer for local editing.\n' +
-  //               '3) Once you\'re done editing your techfolio, it\'s time to put it up online. ' +
-  //               'Simply click "Push Changes to GitHub" in the Config menu.\n' +
-  //               "4) Now you're ready to go! Check out our other Help Menu options for more information.",
-  //     }),
-  //   },
-  //   {
-  //     label: 'Splash Page',
-  //     click: () => dialog.showMessageBox({
-  //       message:
-  //               'The Splash Page has three parts to it: Splash Table, Command Logs, and File Manager.\n' +
-  //               'The Splash Table on the left consists of five rows, which tell you the status of your Techfolio.\n' +
-  //               'The Command Logs below it will show you a log of everything Techfolio Designer does.\n' + // eslint-disable-line
-  //               'The File Manager on the right allows you to manage both your projects and essays.',
-  //     }),
-  //   },
-  //       // todo Projects/essays and image creation. Our design is not finalized as of yet
-  //   {
-  //     label: 'Editing Projects and Essays',
-  //     click: () => dialog.showMessageBox({
-  //       message:
-  //               'todo accessing text editor\n\n' +
-  //               'Once you have created/opened your file for editing, you can write it using Techfolio\'s own text editor.\n' + // eslint-disable-line
-  //               'Techfolio Designer comes with it\'s own TfLint technology, which parses your file for common mistakes most users make.' + // eslint-disable-line
-  //               'TfLint will run when your file is saved (Ctrl/Cmd + S), or by using the keybinding (Ctrl/Cmd + L).',
-  //     }),
-  //   },
-  //   {
-  //     label: 'Adding Images.md',
-  //     click: () => dialog.showMessageBox({
-  //       message: 'todo',
-  //
-  //     }),
-  //   },
-  //   {
-  //     type: 'separator',
-  //   },
-  //   {
-  //     label: 'Shortcuts',
-  //     click: () => dialog.showMessageBox({
-  //       message:
-  //               'Text Editor\n' +
-  //               'Ctrl/Cmd + S: Save\n' +
-  //               'Ctrl/Cmd + L: Run TfLint Essay Checking',
-  //     }),
-  //   },
-  // ];
   return helpSubMenu;
 }
